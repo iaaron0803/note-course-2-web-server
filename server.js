70bf0e61d4d48bccb278cf6e37b24c0709952e6d
iaaron0803 <iaaron0803@gmail.com>
2018-06-09 08:40:21 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const hbs = require('hbs');
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set("view engine", "hbs");
+
+
+app.use((req, res, next) => {
+   var now = new Date().toString();
+   var log = `${now}: ${req.method} ${req.url}`;
+   
+   console.log(log);
+   fs.appendFile('server.log', log + '\n', (err) =>{
+       if(err){
+           console.log('Unable to append to server.log');
+       }
+   });
+   next();
+    
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance', {
+         pageTitle: 'Maintenance Page',
+     });
+    
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+   return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+  return text.toUpperCase();
+});
+
+app.get("/", (req, res) =>{
+  
+    
+    res.render('home',  {
+         pageTitle: 'About Page',
+         welcomeMessage: 'Hello there',
+     });
+});
+
+app.get('/about', (req, res)=>{
+     res.render('about', {
+         pageTitle: 'About Page',
+     });
+});
+
+app.get('/bad', (req, res)=>{
+
+     res.send({
+        erroMessage: 'Error'
+    });
+});
+
+app.get('/help', (req, res)=>{
+     res.render('help');
+});
+
+app.get('/*', (req, res)=>{
+     res.render('maintenance', {
+         pageTitle: 'Maintenance Page',
+     });
+});
+
+
+app.listen(process.env.PORT, process.env.IP, ()=>{
+    console.log("Server Start");
+})
